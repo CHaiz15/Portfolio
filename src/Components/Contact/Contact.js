f336d4ba7d2a0b561c7046c937396e0310aac291
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-05-13 18:23:26 -0600

    Add placeholder for contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,14 +4,23 @@ import styled from 'styled-components'
 const Contact = () => {
   return (
     <ContentContainer>
+      <div>Currently in progress.</div>
+      <div>In the meantime contact me at caleb.haizlett@gmail.com</div>
+      <div>Thanks!</div>
     </ContentContainer>
   )
 }
 
 const ContentContainer = styled.main`
-  background-color: rgb(92,213,4);
+  background-color: rgb(50, 50, 49);
+  flex-direction: column;
   height: 100vh;
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3em;
+  color: rgb(255,255,245);
 `
 
 export default Contact
